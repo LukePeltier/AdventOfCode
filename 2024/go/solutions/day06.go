d3c8002313b7e894e3aee55814264232fcbd491c
Luke Peltier <luke.peltier@microchip.com>
2025-11-10 16:51:01 -0600

    Start day 6 in go

--- 2024/go/solutions/day06.go
+++ 2024/go/solutions/day06.go
@@ -1,5 +1,20 @@
 package solutions
 
+type Direction uint8
+
+const (
+	North Direction = iota
+	East
+	South
+	West
+)
+
+type Coord struct {
+	xpos               uint16
+	ypos               uint16
+	visited_directions []Direction
+}
+
 func Day06Solve(inputFile string) int {
 	// Not yet implemented
 	return 0
